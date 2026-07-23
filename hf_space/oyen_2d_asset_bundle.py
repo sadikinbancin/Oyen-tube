1fe605fe09520ce88fa3464f57b34f22751b332d
sadikinbancin <sadikinbc@gmail.com>
2026-07-23 13:50:59 +0700

    fix: use checksum of committed Oyen 2D asset bundle

--- hf_space/oyen_2d_asset_bundle.py
+++ hf_space/oyen_2d_asset_bundle.py
@@ -12,7 +12,7 @@ from typing import Any
 HERE = Path(__file__).resolve().parent
 PART_DIR = HERE / "oyen_2d_asset_parts"
 EXPECTED_PARTS = 12
-EXPECTED_SHA256 = "f6838cf581bf9f57d418185dc03565cfb2b3148cc6ca96c99f16480a1746e6dd"
+EXPECTED_SHA256 = "095f3bed0cb17e60a53fcfccf65574bb7dc601f423373769e83644e3fe7d3b49"
 MANIFEST_MEMBER = "oyen_2d_layer_manifest.json"
 
 
